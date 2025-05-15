9bc396d5507c8803295747f4b373a6d88ab9d864
root <root@DESKTOP-PTM5FMG.localdomain>
2025-05-15 17:46:25 +0000

    modified java code to stay alive

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -1,8 +1,9 @@
 package com.example;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World from Java in EKS!");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World, This is my first Java app deployed in EKS! with CI-CD");
+        Thread.sleep(Long.MAX_VALUE);  // keeps JVM alive indefinitely
     }
 }
 
